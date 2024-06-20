d7a49e7616e7ae429cecf62472950e9c1426ea1a
Sebastien Cormier <sebastien.cormier@gmail.com>
2024-06-20 15:47:56 +0200

    version initiale

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+import threading
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '192.168.1.39'
+    port = 12345
+    client_socket.connect((host, port))
+
+    pseudo = input("Votre pseudo : ")
+
+    while True:
+        message = input(f"{pseudo}> ")
+        client_socket.sendall((pseudo+"> "+message).encode('utf-8'))
+        data = client_socket.recv(1024)
+        response = data.decode('utf-8')
+        print(f"{response}")
+
+
+if __name__ == "__main__":
+    main()
