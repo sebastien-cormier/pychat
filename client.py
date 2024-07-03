dad1fe4c26e9367e46bd6c6f064f184653e670a7
Sebastien Cormier <sebastien.cormier@gmail.com>
2024-07-03 13:44:12 +0200

    ajout d'un fichier de parametre et de commentaires

--- client.py
+++ client.py
@@ -1,21 +1,34 @@
 import socket
 import threading
+from params import *
 
 def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '192.168.1.29'
-    port = 12346
-    client_socket.connect((host, port))
 
+    # Connection au serveur
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((PARAM_SERVER_IP, PARAM_SERVER_PORT))
+
+    # Saisie du pseudo
     pseudo = input("Votre pseudo : ")
 
+    # Creation de la socket client
+    # TODO
+
+    # Notification des infos clients au serveur (pseudo, socket de reception)
+    # TODO
+
     while True:
+
+        # Saisie d'un message
         message = input(f"{pseudo}> ")
+
+        # Envoi du message
         client_socket.sendall((pseudo+"> "+message).encode('utf-8'))
+
+        # Reception de la réponse
         data = client_socket.recv(1024)
         response = data.decode('utf-8')
         print(f"{response}")
-
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from params import *
 
 list_clients = []
 
@@ -16,38 +17,50 @@ def process_message(param_message) :
 
 def handle_client(client_socket):
     '''
-    fonction qui traite les messages du client
+    fonction qui traite les messages du reçu d'un client
     :param client_socket:
     '''
     while True:
         data = client_socket.recv(1024)
         if not data:
             break
-        message_complet = data.decode('utf-8')
-        pseudo, message = process_message(message_complet)
+        message_avec_pseudo = data.decode('utf-8')
+        pseudo, message = process_message(message_avec_pseudo)
 
+        # Si le client n'est pas listé, on l'ajoute à la liste
         if pseudo not in list_clients:
             list_clients.append(pseudo)
 
         print(f"liste des utilisateurs : {list_clients}")
         print(f"Received message: {message} from {pseudo}")
+
+        # Envoi de ma réponse au client
         response = message
         client_socket.sendall(response.encode('utf-8'))
+
+    # Fermeture de la socket à la fin du traitement du message
     client_socket.close()
 
 
-
 def main():
+    '''
+    Méthode principale
+    :return:
+    '''
+
+    # Ouverture de la socket et ecoute dessus
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '192.168.1.29'
-    port = 12346
-    server_socket.bind((host, port))
+    server_socket.bind((PARAM_SERVER_IP, PARAM_SERVER_PORT))
     server_socket.listen(5)
-    print(f"Server listening on {host}:{port}")
+    print(f"Server listening on {PARAM_SERVER_IP}:{PARAM_SERVER_PORT}")
 
     while True:
+
+        # Reception d'un message d'un client
         client_socket, client_address = server_socket.accept()
         print(f"Accepted connection from {client_address}")
+
+        # On traite le message dans un thread dédié => méthode handle_client
         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
 
